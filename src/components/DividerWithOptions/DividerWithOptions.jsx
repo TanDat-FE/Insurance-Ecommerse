46237cd1b4e884cccf23a95f93a8adf0701ba7a0
TanDat-FE <nguyendat11577@gmail.com>
2025-03-03 10:27:10 +0700

    feat[Login.jsx]: design UI/UX

---
+++ src/components/DividerWithOptions/DividerWithOptions.jsx
@@ -0,0 +1,11 @@
+function DividerWithOptions() {
+  return (
+    <div className="flex justify-center items-center gap-4 mt-8 mb-8 ">
+      <div className="basis-35 border-t border-[#dcdadb]"></div>
+      <div>Or continue with</div>
+      <div className="basis-35 border-t border-[#dcdadb]"></div>
+    </div>
+  );
+}
+
+export default DividerWithOptions;
