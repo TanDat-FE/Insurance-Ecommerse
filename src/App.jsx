4c74b26ea354a7483c41ba1c4e10a9745dfe8538
Thien Nguyen <thienleyi@gmail.com>
2025-03-05 14:48:24 +0700

    HomePage Thien

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Login from "@components/ContentSideBar/Login/Login";
 import "./App.css";
 import MainLayout from "@components/Layout/MainLayout";
+import HomeInf from "@components/InfoInsurace/HomeInf";
+
 
 function App() {
   return (
     <>
       <MainLayout>
-        <Login />
+        <HomeInf />
       </MainLayout>
     </>
   );
