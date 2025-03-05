af617ee0fd54366cc8e884c7a492e5c9b4420d77
vietporo <marcusnora9@gmail.com>
2025-03-05 14:08:53 +0700

    feat(purchase): add UI component for purchase page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Login from "@components/ContentSideBar/Login/Login";
 import "./App.css";
 import MainLayout from "@components/Layout/MainLayout";
+import InsuranceGuide from "@components/purchase/InsuranceGuide";
 
 function App() {
   return (
     <>
       <MainLayout>
-        <Login />
+        {/* <Login /> */}
+        <InsuranceGuide />
       </MainLayout>
     </>
   );
