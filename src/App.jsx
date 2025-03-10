770bedafc1a7e768c9d31ebf6d0dbb03845b7a17
Thien Nguyen <thienleyi@gmail.com>
2025-03-10 19:41:14 +0700

    fix conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from "@components/ContentSideBar/Login/Login";
+import HomeInf from "@components/InfoInsurace/HomeInf";
 import "./App.css";
 import MainLayout from "@components/Layout/MainLayout";
 import Home from "@/Pages/Home/Home";
 
 function App() {
   return (
-    <>
-      <MainLayout>
-        <Home />
-      </MainLayout>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />  {/* Trang Login */}
+        <Route path="/homes" element={<HomeInf/>} />  {/* Trang Home */}
+      </Routes>
+    </Router>
   );
 }
 
