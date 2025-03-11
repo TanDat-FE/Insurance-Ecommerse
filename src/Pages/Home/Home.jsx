5c38dc4dfe642cff72adb37e2bf505b23ad2e39e
LeThiHuynhTrang <lethihuynhtrang0801.com>
2025-03-12 01:04:36 +0700

    feat[Home-page]: reasons

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import Footer from "@/Pages/Footer/Footer";
 import FooterFooter from "@/Pages/Footer/Footer";
 import Contact from "@components/Contact/Contact";
+import Reasons from "@components/Reason/Reasons";
+
 
 function Home() {
   return (
     <div>
+      <Reasons/>
       <Contact />
       <Footer />
     </div>
