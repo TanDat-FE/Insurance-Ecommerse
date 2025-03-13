179e98ebc57d62aca96000b4c409b4456add678e
vietporo <marcusnora9@gmail.com>
2025-03-14 06:43:07 +0700

    feat(section789) done section 7 8, not yet section 9

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,20 @@ import "./App.css";
 import MainLayout from "@components/Layout/MainLayout";
 import InsuranceGuide from "@components/purchase/InsuranceGuide";
 import FAQ from "@components/faqs/FAQs";
+import Section7 from "@components/Section/Section7/section7";
+import Section8 from "@components/Section/Section8/section8";
+import Section9 from "@components/Section/Section9/section9";
 
 function App() {
   return (
     <>
       <MainLayout>
-        <Login />
-        
+        {/* <Login /> */}
+        <Section7 />
+        <Section8 />
+        <Section9 />
+        {/* <FAQ />
+        <InsuranceGuide /> */}
       </MainLayout>
     </>
   );
